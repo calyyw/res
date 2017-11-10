6782361f6ec45a6fe8a6830ad20c5a8b2687b1a3
calyyw <calyyw@163.com>
2017-11-10 13:53:37 +0800

    sql
    sql

---
+++ 123.sql
@@ -0,0 +1,13 @@
+prompt Importing table info...
+set feedback off
+set define off
+insert into info (ID, NAME, JOB, TESTCLOB, TESTDATE)
+values ('5', '°×¹Ç¾«', 'Ñý¹Ö', '<CLOB>', null);
+
+insert into info (ID, NAME, JOB, TESTCLOB, TESTDATE)
+values ('7', 'ºÚÐÜ¹Ö', 'Ñý¹Ö', '<CLOB>', null);
+
+insert into info (ID, NAME, JOB, TESTCLOB, TESTDATE)
+values ('19', 'ºìÌìÀÇ', 'Ñý¹Ö', '<CLOB>', null);
+
+prompt Done.
